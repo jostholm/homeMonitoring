56482a58019b51582d8fd208fbd3f237141ad799
jostholm <joestholm@gmail.com>
2021-04-05 11:01:27 +0200

    correcting some of the structure

--- Prometheus/Client/arm7/temperature/temperature.py
+++ Prometheus/Client/arm7/temperature/temperature.py
@@ -8,16 +8,14 @@ UPDATE_PERIOD = 15
 pi_temperature = prometheus_client.Gauge('system_temperature_celsius','temperature of system in degrees celsius',['location'])
 
 if __name__ == '__main__':
-  prometheus_client.start_http_server(9999)
+    prometheus_client.start_http_server(9999)
 
 while True:
-  temperature = os.system("vcgencmd measure_temp|grep -o -E '[0-9]+\.[0-9]'")
+    temperature = os.system("vcgencmd measure_temp|grep -o -E '[0-9]+\.[0-9]'")
 
     try:
         pi_temperature.labels(location).set(temperature)
-    except ValueError:
-        print("something went wrong setting temperature " temperature )
     except:
-        print("something else went wrong")
+        print("something went wrong with" + temperature)
 
     time.sleep(UPDATE_PERIOD)
