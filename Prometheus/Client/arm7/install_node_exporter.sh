d599c2b5fea9d6b2800557ed72e6e38944555cba
jostholm <joestholm@gmail.com>
2021-04-05 16:17:43 +0200

    Further clean up and small corrections

--- Prometheus/Client/arm7/install_node_exporter.sh
+++ Prometheus/Client/arm7/install_node_exporter.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # install
-wget https://github.com/prometheus/prometheus/releases/download/v2.26.0/prometheus-2.26.0.linux-armv7.tar.gz
+curl -JLO https://github.com/prometheus/node_exporter/releases/download/v1.1.2/node_exporter-1.1.2.linux-armv7.tar.gz
 tar xvfz node_exporter-*.tar.gz
 rm node_exporter-*.tar.gz
 cd node_exporter*armv7 | ./node_exporter
