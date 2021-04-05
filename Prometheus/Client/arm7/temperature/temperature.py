6406b7b0776dd0da12de68bb6aa0b2f0efc417e8
jostholm <joestholm@gmail.com>
2021-04-05 10:48:20 +0200

    Changing how to get temperature to direct call through bash and getting the hostname the same way

--- Prometheus/Client/arm7/temperature/temperature.py
+++ Prometheus/Client/arm7/temperature/temperature.py
@@ -1,9 +1,8 @@
 import prometheus_client
 import time
+import os
 
-location2 = "<hostName>"
-
-
+location = os.system("hostname")
 UPDATE_PERIOD = 15
 
 pi_temperature = prometheus_client.Gauge('system_temperature_celsius','temperature of system in degrees celsius',['location'])
@@ -12,11 +11,13 @@ if __name__ == '__main__':
   prometheus_client.start_http_server(9999)
 
 while True:
-  with open('/home/jostholm/Prometheus/temperature/temp_measure.txt', 'r') as g:
-    try:
-        temperature = g.readline()
-    except:
-        temperature = temperature
+  temperature = os.system("vcgencmd measure_temp|grep -o -E '[0-9]+\.[0-9]'")
 
-    pi_temperature.labels(location2).set(temperature)
+    try:
+        pi_temperature.labels(location).set(temperature)
+    except ValueError:
+        print("something went wrong setting temperature " temperature )
+    except:
+        print("something else went wrong")
+
     time.sleep(UPDATE_PERIOD)
