4482944efbc82cee0e5baf0a454e371ce74a4029
jostholm <81776338+jostholm@users.noreply.github.com>
2022-11-14 23:13:05 +0100

    updated the version of the nodeExporter

--- Prometheus/Client/arm7/Default_Node_Exporter/install_node_exporter.sh
+++ Prometheus/Client/arm7/Default_Node_Exporter/install_node_exporter.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # install
-curl -JLO https://github.com/prometheus/node_exporter/releases/download/v1.1.2/node_exporter-1.1.2.linux-armv7.tar.gz
+curl -JLO https://github.com/prometheus/node_exporter/releases/download/v1.4.0/node_exporter-1.4.0.linux-armv7.tar.gz
 tar xvfz node_exporter-*.tar.gz
 rm node_exporter-*.tar.gz
 cd node_exporter*armv7 | ./node_exporter
