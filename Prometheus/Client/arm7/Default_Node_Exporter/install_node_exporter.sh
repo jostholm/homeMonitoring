bbd8171b053eb6887b0df96f19352c355ba5a74c
jostholm <81776338+jostholm@users.noreply.github.com>
2022-11-14 23:17:51 +0100

    Changed | to && on line 7

--- Prometheus/Client/arm7/Default_Node_Exporter/install_node_exporter.sh
+++ Prometheus/Client/arm7/Default_Node_Exporter/install_node_exporter.sh
@@ -4,4 +4,4 @@
 curl -JLO https://github.com/prometheus/node_exporter/releases/download/v1.4.0/node_exporter-1.4.0.linux-armv7.tar.gz
 tar xvfz node_exporter-*.tar.gz
 rm node_exporter-*.tar.gz
-cd node_exporter*armv7 | ./node_exporter
+cd node_exporter*armv7 && ./node_exporter
